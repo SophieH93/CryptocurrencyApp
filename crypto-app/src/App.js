395991fac0aac8a4717566cb18b9c19b7fa6b750
Sophie <sophiehickey123@gmail.com>
2021-09-27 21:38:55 +0100

    imported navbar to app.js

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Switch, Routh, Link } from "react-router-dom";
-import { layout, Typography, Space } from "antd";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="app">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
