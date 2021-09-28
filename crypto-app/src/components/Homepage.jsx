570f0bc02b5f0e3dbf2f481b8f12ba0a7a1d8715
Sophie <sophiehickey123@gmail.com>
2021-09-28 18:50:25 +0100

    filter crypto to show 10

--- crypto-app/src/components/Homepage.jsx
+++ crypto-app/src/components/Homepage.jsx
@@ -9,7 +9,7 @@ import {Cryptocurrencies, News} from '../components'
 const {Title} = Typography
 
 const Homepage = () => {
-  const {data, isFetching} = useGetCryptosQuery();
+  const {data, isFetching} = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if(isFetching) return 'Loading...';
@@ -38,12 +38,12 @@ const Homepage = () => {
               </Row>
               <div className="home-heading-container">
                 <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
-                <Title level={3} className="show-more"><Link to="/cryptocurrencies"> Top 10 Cryptocurrencies in the world</Link></Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies"> Show More</Link></Title>
               </div>
-              <Cryptocurrencies simplified/>
+              <Cryptocurrencies simplified />
               <div className="home-heading-container">
-                <Title level={2} className="home-title">Latest Crypt News</Title>
-                <Title level={3} className="show-more"><Link to="/news"> Top 10 Cryptocurrencies in the world</Link></Title>
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news"> Show More</Link></Title>
               </div>
               <News simplified />
         </>
