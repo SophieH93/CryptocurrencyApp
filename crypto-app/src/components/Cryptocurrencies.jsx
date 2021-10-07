f81fb70a05a938712db1815c8a734f50f223d98c
Sophie <sophiehickey123@gmail.com>
2021-10-07 22:43:41 +0100

    created Exchanges pg and added loader

--- crypto-app/src/components/Cryptocurrencies.jsx
+++ crypto-app/src/components/Cryptocurrencies.jsx
@@ -4,6 +4,7 @@ import millify from 'millify';
 import {Link} from 'react-router-dom';
 import {Card, Row, Col, Input} from 'antd';
 import {useGetCryptosQuery} from '../services/cryptoApi';
+import Loader from './Loader';
 
 export const Cryptocurrencies = ({simplified}) => {
     const count = simplified ? 10 : 100;
@@ -17,7 +18,7 @@ export const Cryptocurrencies = ({simplified}) => {
         setCryptos(filterData);
     }, [cryptosList, searchTerm])
 
-    if(isFetching) return "Loading.."
+    if(isFetching)  return <Loader />
     return (
         <>
         {!simplified && (
